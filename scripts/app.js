370ab5ac7417f1f9f57a186fb7cc0e8f53e3b39a
Jixiang An <jixiang.an@qq.com>
2017-01-18 16:47:15 +0800

    move files from baidu net disk
    folders: data, fonts, images, scripts, styles, and videos. files: index.html

---
+++ scripts/app.js
@@ -0,0 +1,172 @@
+﻿$(function () {
+    generateHeaderAndFooter();
+    generateNavTopFromXml('top');
+    
+    if (document.head.id != 'index') {
+        generateNavLeftFromXml('left', document.head.id);
+    }
+})
+
+function generateHeaderAndFooter() {
+    var topnav = '<div id="topmain">'
+            + '<div id="top"><div class="logo"><a href="/index.html">'
+                + '<img src="/images/logo.png" alt="海德天润" class="logo" title="海德天润" /></a></div>'
+            + '</div>'
+        + '</div>';
+    var footer = '<div id="footerMain"><div id="footer">版权所有：天津海德天润海上工程技术有限公司</div>';
+    $('div#bodyMain').before(topnav);
+    $('div#bodyMain').after(footer);
+
+    if (document.head.id == 'index') {
+        //var slidePic = '<div class="FocusPic"><div class="content" id="main-slide"><div class="changeDiv">'
+        //                    + '<img src="/images/index/1.jpg" />'
+        //                    + '<img src="/images/index/2.jpg" />'
+        //                    + '<img src="/images/index/3.jpg" />'
+        //                    + '<img src="/images/index/4.jpg" />'
+        //                    + '<img src="/images/index/5.jpg" />'
+        //                    + '<img src="/images/index/6.jpg" />'
+        //            + '</div></div></div><br class="spacer" />';
+        //$('div#body').before(slidePic);
+        //new slide('#main-slide', 960-4, 480-4);
+    }
+    else {
+        var navImage = '<div class="navImage"><img src="/images/index/nav.jpg" /></div>'
+        $('div#body').before(navImage);
+    }
+
+    var ads = '<div id="rightlayoutShow"></div><div style="display:none;" id="rightMessage"></div>';
+    $('div#bodyMain').after(ads);
+}
+
+function generateNavTopFromXml(containerId) {
+    $.get('/data/navbar.xml', function (data) {
+        var html = '<div id="navbar"><ul id="topnav">';
+
+        $(data).find('menu').each(function () {
+            var m = $(this);
+            var target = '';
+            if (m.attr('target')) {
+                target = ' target="_blank"';
+            }
+            html += '<li class="parentlevel"><a href="' + m.attr('href') + '" class="drop_vertical"' + target + '>' + m.attr('text') + '</a>';
+            if (m.find('sub-menu').length > 0) {
+                html += '<div class="dropdown_vertical"><ul>'
+                m.find('sub-menu').each(function () {
+                    var sm = $(this);
+                    if (sm.children('[isshowontop]').length > 0) {
+                        html += '<li class="childlevel"><a href="' + sm.attr('href') + '" class="drop_horizontal">' + sm.attr('text') + '</a>';
+                        html += '<div class="dropdown_horizontal"><ul>';
+                        sm.find('item').each(function () {
+                            var i = $(this);
+                            html += '<li class="childlevel"><a href="' + i.attr('href') + '">' + i.text() + '</a></li>';
+                        });
+                        html += '</ul></div>';
+                    }
+                    else {
+                        html += '<li class="childlevel"><a href="' + sm.attr('href') + '">' + sm.attr('text') + '</a>';
+                    }
+                    html += '</li>';
+                });
+                html += '</ul></div>';
+            }
+            html += '</li>';
+        });
+
+        html += '</ul></div>';
+        $('div#' + containerId).append(html);
+    });
+}
+
+function generateNavLeftFromXml(containerId, groupId) {
+    $.get('/data/navbar.xml', function (data) {
+        var html = '';
+
+        $(data).find('menu').each(function () {
+            var m = $(this);
+            if (m.attr('group') == groupId) {
+                html += '<h1>' + m.attr('text') + '</h1>';
+                m.find('sub-menu').each(function () {
+                    var sm = $(this);
+                    html += '<h2><a href="' + sm.attr('href') + '"><span>' + sm.attr('text') + '</span></a></h2>';
+                    if (sm.children().length > 0) {
+                        html += '<ul>';
+                        sm.find('item').each(function () {
+                            var i = $(this);
+                            html += '<li><a href="' + i.attr('href') + '">+ ' + i.text() + '</a></li>';
+                        });
+                        html += '</ul>';
+                    }
+                });
+            }
+        });
+
+        if (html != '') {
+            $('div#' + containerId).html(html);
+        }
+    });
+}
+
+
+
+
+
+
+
+var returnHtml = '<span class="mini" style="cursor:pointer;"><img src="/images/mini.jpg"></span><span class="zoom" style="cursor:pointer;"></span>';
+    returnHtml += '<div id="iframesrc" style=""></div>';
+//Download by http://www.jb51.net
+$(function() {
+    var boxWidth = 200; //宽度，兼容IE6必写；
+    var boxHeight = 100; //可制空
+    $('#rightlayoutShow').css({ width: boxWidth})
+    $('#rightlayoutShow').prepend(returnHtml);
+    $('#iframesrc').html('<iframe src="/pages/ads.html" scrolling="no" frameborder="0" width="' + boxWidth + ' height="' + boxHeight + '"></iframe>');
+    //这里可以是Iframe Image Flash Text
+    
+    $('#rightlayoutShow .mini').click(function() {
+        $('#iframesrc').slideUp('slow', function() {
+            $('#rightlayoutShow .zoom').html('<img src="/images/close.jpg">');
+            $('#rightlayoutShow .mini').text('');
+        });
+        $('#rightlayoutShow .zoom').css({ display: 'block' });
+        $('#rightlayoutShow .mini').css({ display: 'none' });
+    }); 
+    $('#rightlayoutShow .zoom').click(function() {
+        $('#iframesrc').slideDown('slow', function() {
+            $('#rightlayoutShow .mini').html('<img src="/images/mini.jpg">');
+            $('#rightlayoutShow .zoom').text('');
+        });
+        $('#rightlayoutShow .zoom').css({ display: 'none' });
+        $('#rightlayoutShow .mini').css({ display: 'block' });
+    });
+    $(window).scroll(function() {
+        callScroll('#rightlayoutShow', 10, 10, 'auto', 'absolute');
+    });
+    function callScroll(Sid, Stop, Sright, Swidth, Sposition) {
+        if (IEobject()) {
+            var scrollTop = $(window).scrollTop();
+        }else{
+            var scrollTop = $('#rightlayoutShow').scrollTop();
+        }
+        $('#rightMessage').prepend(scrollTop);
+        $(Sid).css({
+            'bottom' : - (scrollTop - Stop),
+            'right' : Sright,
+            'width' : Swidth,
+            'position' : Sposition
+        });
+    }
+});
+
+function IEobject() {
+    if (window.XMLHttpRequest) {
+        return true; //alert('Mozilla, Safari,IE7 ');
+        if(!window.ActiveXObject){
+            return true; //alert('Mozilla, Safari');
+        } else {
+            return true; //alert('IE7');
+        }
+    } else {
+        return false; //alert('IE6');
+    }
+}
---
+++ scripts/jquery.sgallery.js
@@ -0,0 +1,113 @@
+﻿/*
+ *	sGallery 1.0 - simple gallery with jQuery
+ *	made by bujichong 2009-11-25
+ *	作者：不羁虫  2009-11-25
+ * http://hi.baidu.com/bujichong/
+ *	欢迎交流转载，但请尊重作者劳动成果，标明插件来源及作者
+ */
+
+(function ($) {
+    $.fn.sGallery = function (o) {
+        return new $sG(this, o);
+    };
+
+    var settings = {
+        slideTime: 500,//平滑过渡时间
+        changeTime: 3000,//自动切换时间
+    };
+
+    $.sGalleryLong = function (e, o) {
+        this.options = $.extend({}, settings, o || {});
+        var _self = $(e);
+        var set = this.options;
+        var size = _self.size();
+        var nowIndex = 0; //定义全局指针
+        var index;//定义全局指针
+        var startRun;//预定义自动运行参数
+
+        //初始化
+        _self.eq(0).show();
+
+        //主切换函数
+        function fadeAB() {
+            if (nowIndex != index) {
+                _self.eq(nowIndex).stop(false, true).fadeOut(set.slideTime);
+                _self.eq(index).stop(true, true).fadeIn(set.slideTime);
+                nowIndex = index;
+                clearInterval(startRun);//重置自动切换函数
+                startRun = setInterval(runNext, set.changeTime);
+            }
+        }
+
+        //切换到下一个
+        function runNext() {
+            index = (nowIndex + 1) % size;
+            fadeAB();
+        }
+
+        startRun = setInterval(runNext, set.changeTime);
+    }
+
+    var $sG = $.sGalleryLong;
+})(jQuery);
+
+function slide(Name, Width, Height) {
+    $(Name).width(Width);
+    $(Name).height(Height);
+    $(Name + ' div.changeDiv img').width(Width);
+    $(Name + ' div.changeDiv img').height(Height);
+    $(Name + ' div.changeDiv img').sGallery();
+}
+
+//缩略图
+jQuery.fn.LoadImage = function (scaling, width, height, loadpic) {
+    if (loadpic == null) loadpic = "/images/loading.gif";
+    return this.each(function () {
+        var t = $(this);
+        var src = $(this).attr("src")
+        var img = new Image();
+        img.src = src;
+        //自动缩放图片
+        var autoScaling = function () {
+            if (scaling) {
+                if (img.width > 0 && img.height > 0) {
+                    if (img.width / img.height >= width / height) {
+                        if (img.width > width) {
+                            t.width(width);
+                            t.height((img.height * width) / img.width);
+                        } else {
+                            t.width(img.width);
+                            t.height(img.height);
+                        }
+                    }
+                    else {
+                        if (img.height > height) {
+                            t.height(height);
+                            t.width((img.width * height) / img.height);
+                        } else {
+                            t.width(img.width);
+                            t.height(img.height);
+                        }
+                    }
+                }
+            }
+        }
+        //处理ff下会自动读取缓存图片
+        if (img.complete) {
+            autoScaling();
+            return;
+        }
+        $(this).attr("src", "");
+        var loading = $("<img alt=\"加载中...\" title=\"图片加载中...\" src=\"" + loadpic + "\" />");
+
+        t.hide();
+        t.after(loading);
+        $(img).load(function () {
+            autoScaling();
+            loading.remove();
+            t.attr("src", this.src);
+            t.show();
+        });
+    });
+}
+
